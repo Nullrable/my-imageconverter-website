c1e7659b8de931969b8b5683859515edd8852056
nhsoft.lsd <nhsoft.lsd@hotmail.com>
2026-03-15 19:24:57 +0800

    website

---
+++ config.js
@@ -0,0 +1,11 @@
+// App 配置
+window.APP_CONFIG = {
+    // App Store 下载地址
+    appStoreUrl: 'https://apps.apple.com/us/app/image-converter-photo-pdf/id6760528510',
+    // 隐私协议地址
+    privacyPolicyUrl: 'https://docs.google.com/document/d/1j5wlctDOAQ409uXKGpecw-Vudzz3kfohgj2OA8sjNbU/edit?usp=drive_link',
+    // 联系邮箱
+    contactEmail: 'nhsoft.lsd@gmail.com',
+    // App 名称
+    appName: 'Image Converter'
+};
